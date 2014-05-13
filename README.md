c6f27b7bc1e9e862fb22e9de05ae0421b204780f
Anthony Eden <anthonyeden@gmail.com>
2014-05-13 17:47:13 +0200

    Initial extraction from erldns.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Metrics API for erldns
+
+This app provides an API for gathering and querying metrics from an erldns server.
+
---
+++ src/erldns_metrics_app.erl
@@ -0,0 +1,29 @@
+%% Copyright (c) 2012-2014, Aetrion LLC
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+%% @doc The erldns OTP application.
+-module(erldns_metrics_app).
+-behavior(application).
+
+% Application hooks
+-export([start/2, stop/1]).
+
+start(_Type, _Args) ->
+  lager:debug("Starting erldns_metrics application"),
+  erldns_metrics_sup:start_link().
+
+stop(_State) ->
+  lager:info("Stop erldns_metrics application"),
+  ok.
+
