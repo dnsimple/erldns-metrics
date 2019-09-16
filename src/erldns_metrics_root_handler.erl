b13e95f87309f7a85d5400cf2ac4c00d56f1585e
Anthony Eden <me@anthonyeden.com>
2019-09-16 14:57:55 -0400

    Update handler init function

--- src/erldns_metrics_root_handler.erl
+++ src/erldns_metrics_root_handler.erl
@@ -15,12 +15,14 @@
 %% @doc Cowboy handler for the Metrics API endpoint /
 -module(erldns_metrics_root_handler).
 
--export([init/3]).
+-export([init/2]).
 -export([content_types_provided/2]).
 -export([to_html/2, to_json/2, to_text/2]).
 
-init(_Transport, _Req, []) ->
-  {upgrade, protocol, cowboy_rest}.
+-behaviour(cowboy_rest).
+
+init(Req, State) ->
+  {cowboy_rest, Req, State}.
 
 content_types_provided(Req, State) ->
   {[
